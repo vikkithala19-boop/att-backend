4902db3f0cfc5970324350c8f4b1360260c5f7b9
vikkithala19-boop <vikkithala19@gmail.com>
2026-03-14 22:54:15 +0530

    Update fetch_courses.py

--- fetch_courses.py
+++ fetch_courses.py
@@ -2,10 +2,17 @@ from bs4 import BeautifulSoup
 
 COURSEPLAN_URL = "https://ecampus.psgtech.ac.in/studzone/Attendance/courseplan"
 
+# cache
+_cached_subjects = None
+
 def fetch_courses(session):
+    global _cached_subjects
+
+    # if already fetched, reuse
+    if _cached_subjects is not None:
+        return _cached_subjects
 
     res = session.get(COURSEPLAN_URL)
-
     soup = BeautifulSoup(res.text, "html.parser")
 
     subject_map = {}
@@ -14,10 +21,9 @@ def fetch_courses(session):
     names = soup.select(".coursename")
 
     for c, n in zip(codes, names):
-
         code = c.text.strip()
         name = n.text.strip()
-
         subject_map[code] = name
 
+    _cached_subjects = subject_map
     return subject_map
