14889626fbff424bdf010fc4c8687f1d56fe7eee
vikkithala19-boop <vikkithala19@gmail.com>
2026-03-14 19:25:33 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,70 +1,86 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import requests
-from bs4 import BeautifulSoup
+Request URL
+https://www.google-analytics.com/g/collect?v=2&tid=G-G3M5RQWDTC&gtm=45je63b1v9114009267za20g&_p=1773495537030&gcd=13l3l3l3l1l1&npa=0&dma=0&cid=2014095914.1756471467&ul=en-us&sr=1536x864&uaa=x86&uab=64&uafvl=Chromium%3B146.0.7680.76%7CNot-A.Brand%3B24.0.0.0%7CGoogle%2520Chrome%3B146.0.7680.76&uamb=0&uam=&uap=Windows&uapv=19.0.0&uaw=0&are=1&frm=0&pscdl=noapi&_eu=AAAIAAQ&_s=1&tag_exp=103116026~103200004~115938466~115938469~116024733~117484252&sid=1773495475&sct=6&seg=1&dl=https%3A%2F%2Fbunker-api.vercel.app%2F&dr=https%3A%2F%2Fbunker-api.vercel.app%2F&dt=Bunker&en=page_view&tfd=8411
+Request Method
+POST
+Status Code
+204 No Content
+Remote Address
+142.250.77.142:443
+Referrer Policy
+strict-origin-when-cross-origin
+access-control-allow-credentials
+true
+access-control-allow-origin
+https://bunker-api.vercel.app
+alt-svc
+h3=":443"; ma=2592000,h3-29=":443"; ma=2592000
+cache-control
+no-cache, no-store, must-revalidate
+content-length
+0
+content-security-policy-report-only
+script-src 'none'; form-action 'none'; frame-src 'none'; report-uri https://csp.withgoogle.com/csp/scaffolding/ascnsrsggc:113:0
+content-type
+text/plain
+cross-origin-opener-policy-report-only
+same-origin; report-to=ascnsrsggc:113:0
+cross-origin-resource-policy
+cross-origin
+date
+Sat, 14 Mar 2026 13:39:01 GMT
+expires
+Fri, 01 Jan 1990 00:00:00 GMT
+pragma
+no-cache
+report-to
+{"group":"ascnsrsggc:113:0","max_age":2592000,"endpoints":[{"url":"https://csp.withgoogle.com/csp/report-to/scaffolding/ascnsrsggc:113:0"}],}
+server
+Golfe2
+:authority
+www.google-analytics.com
+:method
+POST
+:path
+/g/collect?v=2&tid=G-G3M5RQWDTC&gtm=45je63b1v9114009267za20g&_p=1773495537030&gcd=13l3l3l3l1l1&npa=0&dma=0&cid=2014095914.1756471467&ul=en-us&sr=1536x864&uaa=x86&uab=64&uafvl=Chromium%3B146.0.7680.76%7CNot-A.Brand%3B24.0.0.0%7CGoogle%2520Chrome%3B146.0.7680.76&uamb=0&uam=&uap=Windows&uapv=19.0.0&uaw=0&are=1&frm=0&pscdl=noapi&_eu=AAAIAAQ&_s=1&tag_exp=103116026~103200004~115938466~115938469~116024733~117484252&sid=1773495475&sct=6&seg=1&dl=https%3A%2F%2Fbunker-api.vercel.app%2F&dr=https%3A%2F%2Fbunker-api.vercel.app%2F&dt=Bunker&en=page_view&tfd=8411
+:scheme
+https
+accept
+*/*
+accept-encoding
+gzip, deflate, br, zstd
+accept-language
+en-US,en;q=0.9
+content-length
+0
+cookie
+ar_debug=1
+origin
+https://bunker-api.vercel.app
+priority
+u=1, i
+referer
+https://bunker-api.vercel.app/
+sec-ch-ua
+"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"
+sec-ch-ua-mobile
+?0
+sec-ch-ua-platform
+"Windows"
+sec-fetch-dest
+empty
+sec-fetch-mode
+no-cors
+sec-fetch-site
+cross-site
+sec-fetch-storage-access
+active
+user-agent
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36
+MCP server
+CrUX data in performance trace summaries, a new --slim mode, specialized skills and experimental screen recording support.
 
-app = Flask(__name__)
-CORS(app)
+Preserve Console history edits
+Modify a previous command, browse for another one, and return to your draft without losing your work.
 
-LOGIN_URL = "https://ecampus.psgtech.ac.in/studzone"
-ATT_URL = "https://ecampus.psgtech.ac.in/studzone/Attendance/StudentPercentage"
-
-@app.route("/")
-def home():
-    return "Attendance API Running"
-
-
-@app.route("/attendance", methods=["POST"])
-def attendance():
-
-    try:
-
-        data = request.json
-        student_id = data.get("id")
-        password = data.get("password")
-
-        session = requests.Session()
-
-        # LOGIN REQUEST
-        payload = {
-            "Rollno": student_id,
-            "Password": password
-        }
-
-        login = session.post(LOGIN_URL, data=payload)
-
-        # GET ATTENDANCE PAGE
-        page = session.get(ATT_URL)
-
-        soup = BeautifulSoup(page.text, "html.parser")
-
-        # FIND TABLE ROWS
-        rows = soup.select("#StudentPercentageTable tbody tr")
-
-        subjects = []
-
-        for r in rows:
-
-            cols = [c.text.strip() for c in r.find_all("td")]
-
-            if len(cols) >= 6:
-
-                subjects.append({
-                    "course": cols[0],
-                    "total": cols[1],
-                    "absent": cols[3],
-                    "present": cols[4],
-                    "percent": cols[5]
-                })
-
-        return jsonify(subjects)
-
-    except Exception as e:
-
-        return jsonify({
-            "error": str(e)
-        })
-
-
-if __name__ == "__main__":
-    app.run()
+Improved support for adopted stylesheets
+Adopted Style Sheets are now grouped under a dedicated #adopted-style-sheets node within the DOM tree in the Elements panel.
